c01844f1b5ea6a34ae3817a59fa7f3fba29ef506
JordanBell98 <104156844+JordanBell98@users.noreply.github.com>
2022-11-17 18:48:15 -0700

    Adding collision file

---
+++ collisions.py
@@ -0,0 +1,118 @@
+import constants
+from game.casting.actor import Actor
+from game.scripting.action import Action
+from game.shared.point import Point
+
+class HandleCollisionsAction(Action):
+    """
+    An update action that handles interactions between the actors.
+    
+    The responsibility of HandleCollisionsAction is to handle the situation when the player_1 collides
+    with the food, or the player_1 collides with its segments, or the game is over.
+
+    Attributes:
+        _is_game_over (boolean): Whether or not the game is over.
+    """
+
+    def __init__(self):
+        """Constructs a new HandleCollisionsAction."""
+        self._is_game_over = False
+
+    def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            self._handle_food_collision(cast)
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
+
+    def _handle_player_1_collision(self, cast):
+        """Updates the score and ends game if colission.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        score = cast.get_first_actor("scores")
+        player_2 = cast.get_first_actor("player_2")
+        player_1 = cast.get_first_actor("player_1")
+        head = player_1.get_head()
+
+        if head.get_position().equals(player_2.get_position()):
+            points = player_2.get_points()
+    
+    def _handle_player_2_collision(self, cast):
+        """Updates the score and ends game if colission.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        score = cast.get_first_actor("scores")
+        player_2 = cast.get_first_actor("player_2")
+        player_1 = cast.get_first_actor("player_1")
+        head = player_2.get_head()
+
+        if head.get_position().equals(player_1.get_position()):
+            points = player_1.get_points()
+            
+    
+    def _handle_segment_collision(self, cast):
+        """Sets the game over flag if the player_1 collides with one of its segments.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        player_1 = cast.get_first_actor("player_1")
+        head = player_1.get_segments()[0]
+        segments = player_1.get_segments()[1:]
+        
+        
+        for segment in segments:
+            if head.get_position().equals(segment.get_position()):
+                self._is_game_over = True
+        
+    def _handle_segment_2_collision(self, cast):
+        """Sets the game over flag if the player_1 collides with one of its segments.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        player_2 = cast.get_first_actor("player_2")
+        head_2 = player_2.get_segments()[0]
+        segments_2 = player_2.get_segments()[1:]
+
+        for segment_2 in segments_2:
+            if head_2.get_position().equals(segment_2.get_position()):
+                self._is_game_over = True
+        
+
+    def _handle_game_over(self, cast):
+        """Shows the 'game over' message and turns the player_1 and food white if the game is over.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        if self._is_game_over:
+            player_1 = cast.get_first_actor("player_1")
+            segments = player_1.get_segments()
+            player_2 = cast.get_first_actor("player_2")
+            segments_2 = player_2.get_segments()
+
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+            position = Point(x, y)
+
+            message = Actor()
+            message.set_text("Game Over!")
+            message.set_position(position)
+            cast.add_actor("messages", message)
+
+            for segment in segments:
+                segment.set_color(constants.WHITE)
+            for segment_2 in segments_2:
+                segment_2.set_color(constants.WHITE)
+    
+    
